8ea3132d90178225ca9b3f8e3489669634baf77d
Alvin Ababil <neoryumasil@gmail.com>
2025-08-13 11:29:31 +0800

    Fix Slime bug

--- splittedrealms/Scripts/slime.gd
+++ splittedrealms/Scripts/slime.gd
@@ -1,78 +1,93 @@
 extends CharacterBody2D
 
+# Attribut Slime
 var SPEED = 80
 var health = 4
 
+# Attribut Knockback
 var knockback_force = Vector2.ZERO
 var knockback_duration = 0.2
 var knockback_timer = 0.0
 
+# Nodes
 var player: Node2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var chase = false
 
+# Booleans
+var chase = false
+var is_hit = false
 
 func _ready():
+	# Set animasi dan player
 	$Animation.play("Idle")
 	var players = get_tree().get_nodes_in_group("player")
 	if players.size() > 0:
 		player = players[0]
 
 func _physics_process(delta):
+	# Set Gravity Physics
 	velocity.y += gravity * delta
-
-	if chase and player and $Animation.animation != "Death" and $Animation.animation != "Hit":
-		$Animation.play("Walk")
-		var direction = (player.global_position - global_position).normalized()
-
-		if direction.x < 0:
-			$Animation.flip_h = false
-		else:
-			$Animation.flip_h = true
-
-		velocity.x = direction.x * SPEED
-	else:
-		if $Animation.animation != "Death" and $Animation.animation != "Hit":
-			$Animation.play("Idle")
-		velocity.x = 0
-
+	
+	# Death
 	if health <= 0:
 		death()
-	
+		return
+
+	# Knockback Physics
 	if knockback_timer > 0:
 		velocity = knockback_force
 		knockback_timer -= delta
-	else :
-		velocity = Vector2.ZERO
+	else:
+		# Hit Physics
+		if is_hit:
+			velocity.x = 0
+		elif chase and player:
+			# Pergerakan
+			var direction = (player.global_position - global_position).normalized()
+			velocity.x = direction.x * SPEED
+
+			# Flip animasi sesuai arah
+			$Animation.flip_h = direction.x > 0
+
+			# Animasi Jalan
+			if abs(velocity.x) > 10:
+				$Animation.play("Walk")
+			else:
+				$Animation.play("Idle")
+		else:
+			velocity.x = 0
+			$Animation.play("Idle")
 
 	move_and_slide()
 
+
+func _on_hit_zone_area_entered(area: Area2D) -> void:
+	# Mekanik kena projectile
+	if area.name == "Projectile":
+		health -= 3
+		is_hit = true
+		$Animation.play("Hit")
+		
+		# Mekanik Knockback
+		var direction = (global_position - area.global_position).normalized()
+		knockback_force = direction * 300
+		knockback_timer = knockback_duration
+
+		await $Animation.animation_finished
+		is_hit = false
+
+# Mekanik deteksi Player
 func _on_player_detector_body_entered(body):
 	if body.name == "Player":
 		chase = true
 
 func _on_player_detector_body_exited(body):
 	if body.name == "Player":
-		chase = false	
-		
+		chase = false
+
+# Meninggal
 func death():
 	velocity.x = 0
 	$Animation.play("Death")
 	await $Animation.animation_finished
 	queue_free()
-
-func _on_attack_area_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		PlayerStatus.HP -= 1
-
-func _on_hit_zone_area_entered(area: Area2D) -> void:
-	if area.name == "Projectile":
-		$Animation.play("Hit")
-		health -= 3
-		
-		var direction = (global_position - area.global_position).normalized()
-		knockback_force = direction * 300
-		knockback_timer = knockback_duration
-		
-		await $Animation.animation_finished
-		
