e23fa289cdbaf58e62d13d0164fa5c35b99a86a4
Alvin Ababil <neoryumasil@gmail.com>
2025-08-13 10:53:45 +0800

    Fix Slime Mechanic

--- splittedrealms/Scripts/slime.gd
+++ splittedrealms/Scripts/slime.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
 var SPEED = 80
+var health = 4
+
+var knockback_force = Vector2.ZERO
+var knockback_duration = 0.2
+var knockback_timer = 0.0
+
 var player: Node2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var chase = false
-var health = 4
+
 
 func _ready():
 	$Animation.play("Idle")
@@ -15,7 +21,7 @@ func _ready():
 func _physics_process(delta):
 	velocity.y += gravity * delta
 
-	if chase and player and $Animation.animation != "Death":
+	if chase and player and $Animation.animation != "Death" and $Animation.animation != "Hit":
 		$Animation.play("Walk")
 		var direction = (player.global_position - global_position).normalized()
 
@@ -26,12 +32,18 @@ func _physics_process(delta):
 
 		velocity.x = direction.x * SPEED
 	else:
-		if $Animation.animation != "Death":
+		if $Animation.animation != "Death" and $Animation.animation != "Hit":
 			$Animation.play("Idle")
 		velocity.x = 0
 
 	if health <= 0:
 		death()
+	
+	if knockback_timer > 0:
+		velocity = knockback_force
+		knockback_timer -= delta
+	else :
+		velocity = Vector2.ZERO
 
 	move_and_slide()
 
@@ -41,13 +53,8 @@ func _on_player_detector_body_entered(body):
 
 func _on_player_detector_body_exited(body):
 	if body.name == "Player":
-		chase = false
-
-func _on_hit_zone_body_entered(body):
-	if body.name == "Projectile":
-		$Animation.play("Hit")
-		health -= 3
-
+		chase = false	
+		
 func death():
 	velocity.x = 0
 	$Animation.play("Death")
@@ -56,5 +63,16 @@ func death():
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		$Animation.play("Attack")
 		PlayerStatus.HP -= 1
+
+func _on_hit_zone_area_entered(area: Area2D) -> void:
+	if area.name == "Projectile":
+		$Animation.play("Hit")
+		health -= 3
+		
+		var direction = (global_position - area.global_position).normalized()
+		knockback_force = direction * 300
+		knockback_timer = knockback_duration
+		
+		await $Animation.animation_finished
+		
